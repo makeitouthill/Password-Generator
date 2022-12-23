6e90d1a2327f6b496c42c5e7eaf92bbc7ae34b0a
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2022-12-22 22:37:57 -0800

    Add functions to generate password and deleted function under write password

--- Develop/script.js
+++ Develop/script.js
@@ -4,41 +4,65 @@ var includeLowercase;
 var includeUppercase;
 var includeNumbers;
 var includeSpecial;
+var lowerCases = Array.from("abcdefghijklmnopqrstuvwxyz");
+var upperCases = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var numbers = Array.from("1234567890");
+var specials = Array.from("!@#$%^&*?");
 
-function promptUser(){
-  pwLength = prompt("Enter Password Length (8-128 Characters):")
-  includeLowercase = confirm("Include Lowercases characters?:")
-  includeUppercase = confirm("Include Lowercases characters?:")
-  includeNumbers = confirm("Include Numbers?:")
-  includeSpecial = confirm("Include Special Characters?:")
+function promptUser() {
+  pwLength = parseInt(prompt("Enter Password Length (8-128 Characters):"));
+  includeLowercase = confirm("Include Lowercase characters?");
+  includeUppercase = confirm("Include Uppercase characters?");
+  includeNumbers = confirm("Include Numbers?");
+  includeSpecial = confirm("Include Special Characters?");
 }
 
-function validEntry(){
-if(pwLength < 8 || pwLength > 128){
-  alert("Password length must be between 8 and 128 characters!");
+function validEntry() {
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Password length must be between 8 and 128 characters!");
     return false;
-}
-  if (!includeLowercase && !includeUppercase && !includeNumbers
-  && !includeSpecial){
-    alert("Must select atleast one character type to include in the password!");
+  }
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
+    alert("You must select at least one character type to include in the password.");
     return false;
-   } 
-   return true;
+  }
+  return true;
 }
+
+  function generatePassword() {
+
+    let password = "";
+  
+    for (let i = 0; i < pwLength; i++) {
+      if (includeLowercase) {
+        password += getRandomChar(lowerCases);
+      }
+      if (includeUppercase) {
+        password += getRandomChar(upperCases);
+      }
+      if (includeNumbers) {
+        password += getRandomChar(numbers);
+      }
+      if (includeSpecial) {
+        password += getRandomChar(specials);
+      }
+    }
+  
+    return password;
+  }
+  
+  function getRandomChar(charSet) {
+    let index = Math.floor(Math.random() * charSet.length);
+    return charSet[index];
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(){
-    if (validEntry === true) {
-      generatePassword.passwordText
-      
-    }
-  };
+function writePassword(){
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
