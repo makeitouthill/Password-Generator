f24eec71d60cf7e1704b98872051f817605786dd
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2022-12-22 23:24:49 -0800

    Added character count and changed for loop to while loop. Added eventListener if condition to check if the entry was valid to write the password

--- Develop/script.js
+++ Develop/script.js
@@ -28,47 +28,54 @@ function validEntry() {
   }
   return true;
 }
-
   function generatePassword() {
-
     let password = "";
+    let charCount = 0;
   
-    for (let i = 0; i < pwLength; i++) {
-      if (includeLowercase) {
+    while (charCount < pwLength) {
+      if (includeLowercase && charCount < pwLength) {
         password += getRandomChar(lowerCases);
+        charCount++;
       }
-      if (includeUppercase) {
+      if (includeUppercase && charCount < pwLength) {
         password += getRandomChar(upperCases);
+        charCount++;
       }
-      if (includeNumbers) {
+      if (includeNumbers && charCount < pwLength) {
         password += getRandomChar(numbers);
+        charCount++;
       }
-      if (includeSpecial) {
+      if (includeSpecial && charCount < pwLength) {
         password += getRandomChar(specials);
+        charCount++;
       }
     }
   
     return password;
   }
-  
+
   function getRandomChar(charSet) {
     let index = Math.floor(Math.random() * charSet.length);
     return charSet[index];
   }
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword(){
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", function(){
-  promptUser();
-});
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    if (validEntry()) {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", function() {
+    promptUser();
+    if (validEntry()) {
+      writePassword();
+    }
+  });
+  generateBtn.addEventListener("click", writePassword); 
